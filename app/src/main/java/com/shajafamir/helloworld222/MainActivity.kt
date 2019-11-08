172ce47a19fd8fbec66cff6d2c40f6ed700988b7
Programmer1234 <shahafgonen73142@gmail.com>
2019-11-08 21:10:12 +0200

    Added another button that opens the activity of calling number

--- app/src/main/java/com/shajafamir/helloworld222/MainActivity.kt
+++ app/src/main/java/com/shajafamir/helloworld222/MainActivity.kt
@@ -5,6 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.net.Uri.fromParts
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.net.Uri
+
 
 const val EXTRA_MESSAGE = "com.shajafamir.helloworld222.MESSAGE"
 
@@ -23,5 +30,12 @@ class MainActivity : AppCompatActivity() {
             putExtra(EXTRA_MESSAGE, message)
         }
         startActivity(intent)
+    }
+
+    /** Called when user taps lower button */
+    fun dialContactPhone(view: View) {
+        val editText = findViewById<EditText>(R.id.editText)
+            val phoneNumber = editText.text.toString()
+        startActivity(Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phoneNumber, null)))
     }
 }
