d89cb10ccd6ee8f1ad280ba62becd64f5dc70e59
Programmer1234 <shahafgonen73142@gmail.com>
2019-11-08 22:32:47 +0200

    Can call phone.

---
+++ app/src/main/java/com/shajafamir/helloworld222/DisplayNoPermissionsActivity.kt
@@ -0,0 +1,12 @@
+package com.shajafamir.helloworld222
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DisplayNoPermissionsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_no_permissions)
+    }
+}
--- app/src/main/java/com/shajafamir/helloworld222/MainActivity.kt
+++ app/src/main/java/com/shajafamir/helloworld222/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.shajafamir.helloworld222
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -11,11 +14,18 @@ import androidx.core.app.ComponentActivity.ExtraData
 import androidx.core.content.ContextCompat.getSystemService
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.net.Uri
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 
 const val EXTRA_MESSAGE = "com.shajafamir.helloworld222.MESSAGE"
 
+@SuppressLint("ByteOrderMark")
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "PermissionDemo"
+    private val CALL_REQUEST_CODE = 101
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +45,33 @@ class MainActivity : AppCompatActivity() {
     /** Called when user taps lower button */
     fun dialContactPhone(view: View) {
         val editText = findViewById<EditText>(R.id.editText)
-            val phoneNumber = editText.text.toString()
-        startActivity(Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phoneNumber, null)))
+        val phoneNumber = editText.text.toString()
+        //startActivity(Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phoneNumber, null)))
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            //val noPermIntent = Intent(this, DisplayNoPermissionsActivity::class.java).apply {}
+            //startActivity(noPermIntent)
+            Log.i(TAG, "Permission to record denied")
+            ActivityCompat.requestPermissions(this,  arrayOf(Manifest.permission.CALL_PHONE),  CALL_REQUEST_CODE)
+        } else {
+            val callIntent =
+                Intent(Intent.ACTION_CALL, Uri.fromParts("tel", phoneNumber, null)).apply {}
+            startActivity(callIntent)
+        }
     }
+    /**
+    ﻿override fun onRequestPermissionsResult(requestCode: Int,
+                                             permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            CALL_REQUEST_CODE -> {
+
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+
+                    Log.i(TAG, "Permission has been denied by user")
+                } else {
+                    Log.i(TAG, "Permission has been granted by user")
+                }
+            }
+        }
+    }﻿*/
 }
