5a57e9e4cddf2596fd3b92942ebf84ee5316a900
amir1902 <amir.192@gmail.com>
2019-11-09 19:49:20 +0200

    add save gate number and white list

--- app/src/main/java/com/shajafamir/helloworld222/MainActivity.kt
+++ app/src/main/java/com/shajafamir/helloworld222/MainActivity.kt
@@ -15,8 +15,10 @@ import androidx.core.content.ContextCompat.getSystemService
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.net.Uri
 import android.util.Log
+import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import android.content.Context
 
 
 const val EXTRA_MESSAGE = "com.shajafamir.helloworld222.MESSAGE"
@@ -27,11 +29,14 @@ class MainActivity : AppCompatActivity() {
     private val TAG = "PermissionDemo"
     private val CALL_REQUEST_CODE = 101
     private val requestUserPermission = 2
+    private val PREFS_NAME = "kotlincodes"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sharedPreference:SharedPreference=SharedPreference(this)
 
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ANSWER_PHONE_CALLS)
             != PackageManager.PERMISSION_GRANTED) {
@@ -70,5 +75,44 @@ class MainActivity : AppCompatActivity() {
                 Intent(Intent.ACTION_CALL, Uri.fromParts("tel", phoneNumber, null)).apply {}
             startActivity(callIntent)
         }
+    }
+
+    fun saveGateNumber(view: View) {
+        val editText = findViewById<EditText>(R.id.editText4)
+        val gateNumber = editText.text.toString()
+        val sharedPreference:SharedPreference=SharedPreference(this)
+        sharedPreference.save("gateNumber",gateNumber)
+    }
+
+    fun displayGateNumber(view: View) {
+        val sharedPreference:SharedPreference=SharedPreference(this)
+        val gateNumber = sharedPreference.getValueString("gateNumber")
+
+        setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.gateNumberView)
+        textView.text = gateNumber
+        println(gateNumber)
+
+        val sharedPreferences = this.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        val sharedPreferenceIds = sharedPreferences.all.map { it.key } //returns List<String>
+        println(sharedPreferenceIds[0])
+    }
+
+    fun approveGuest(view: View) {
+        val editText1 = findViewById<EditText>(R.id.approvedName)
+        val name = editText1.text.toString()
+        val editText2 = findViewById<EditText>(R.id.approvedNumber)
+        val number = editText2.text.toString()
+        val sharedPreference: SharedPreference = SharedPreference(this)
+        val sharedPreferences = this.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        val sharedPreferenceIds = sharedPreferences.all.map { it.key } //returns List<String>
+
+        if (name in sharedPreferenceIds) {
+            sharedPreference.removeValue(name)
+            sharedPreference.save(name, number)
+        } else {
+            sharedPreference.save(name, number)
+        }
+
     }
 }
